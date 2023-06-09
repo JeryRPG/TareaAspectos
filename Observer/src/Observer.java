ef0affef6818f7d3db147da7fdca817f267f9b5d
JeryRPG <jerypovedag@gmail.com>
2023-06-08 22:02:30 -0500

    Ímplementación del JavaFX básico

--- Observer/src/Observer.java
+++ Observer/src/Observer.java
@@ -1,7 +1,29 @@
-public class Observer {
-	public static void main(String[] args) 
-	{
-		// Código Base
-		System.out.println("Test");	
-	}
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Observer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+
+        Button botonRojo = new  Button("Rojo");
+        Button botonVerde = new Button("Verde");
+        Button botonAzul = new Button("Azul");
+
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(botonRojo, botonVerde, botonAzul);
+
+        primaryStage.setScene(new Scene(vbox, 200, 150));
+        primaryStage.show();
+    }
 }

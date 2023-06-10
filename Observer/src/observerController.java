fa058b9d0e53ad0eadfe9f7c2d9df76657bdad1b
jcvivas <jcvivas@espol.edu.ec>
2023-06-09 22:32:11 -0500

    funcion de cambiar fondo al aplastar los botones

--- Observer/src/observerController.java
+++ Observer/src/observerController.java
@@ -1,5 +1,10 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class observerController {
 
@@ -11,19 +16,24 @@ public class observerController {
 
     @FXML
     private Button botonVerde;
+    @FXML
+    private VBox rootPane;
 
     @FXML
-    private void botonRojoClick() {
+    private void botonRojoClick(ActionEvent event) {
         System.out.println("Se ha hecho clic en el botón Rojo");
+        rootPane.setStyle("-fx-background-color:  lightCoral;");
     }
 
     @FXML
     private void botonVerdeClick() {
         System.out.println("Se ha hecho clic en el botón Verde");
+        rootPane.setStyle("-fx-background-color: lightGreen;");
     }
 
     @FXML
     private void botonAzulClick() {
         System.out.println("Se ha hecho clic en el botón Azul");
+        rootPane.setStyle("-fx-background-color: lightBlue;");
     }
 }
